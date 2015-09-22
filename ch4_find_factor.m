ddab853a80658713f92a62944b22f00bcd86aff1
yiyang59 <yiyang59@hotmail.com>
2015-09-22 00:13:22 -0400

    basic anal

--- ch4_find_factor.m
+++ ch4_find_factor.m
@@ -1,8 +1,8 @@
 clear;
 
-%Global parameters (assumptions)
+%%%%%%%%%%%Global parameters (assumptions)
 minAccountingLag = 3; %assume 3 months gap in accounting reports.
-maxAccountingLag = 5;
+maxAccountingLag = 5; %Cannot carryover for > 5 months.
 
 %load all data.
 col_seq=1;           col_ticker=2;     col_month=3;  col_pricedate=4; 
@@ -17,11 +17,121 @@ alldata = textscan (fid,'%d%s%d%s%f%s%f%f%f%f%f%f%f%f%f%f',      ...
           'Delimiter',',','Headerlines',1,'TreatAsEmpty','NA');
 fclose(fid);
 
-%Data cleaning and preparation
+%%%%%%%%%%%% Data cleaning and preparation
 price = alldata{col_pricem};
-ticker = alldata{1};
+ticker = alldata{col_ticker};
 
-for i = 1 .. length(alldata{5}) - 1
-    if (strcmp(
+NumRow = length(price);
+
+priceReturn = NaN(NumRow,1);
+for i = 1 : NumRow - 1
+    if (strcmp(ticker(i), ticker(i+1)))
+        ret = (price(i+1) - price(i)) / price(i);
+    else
+        ret = nan;
+    end
+    priceReturn(i) = ret;
 end
-priceReturn =  
+
+%%Fill accounting values, from quarterly to monthly 1->3 months.
+lastAccountingRow = 0;
+lastMonthNumber = 0;
+lastTicker = '';
+for i = 1 : NumRow
+    if (~strcmp(alldata{col_acctdate}(i), 'NA'))
+        lastAccountingRow = i;
+        lastMonthNumber = alldata{col_month}(i);
+        lastTicker = alldata{col_ticker}(i);
+    else
+        currentMonth = alldata{col_month}(i);
+        currentTicker = alldata{col_ticker}(i);
+        if (currentMonth <= lastMonthNumber + maxAccountingLag && ...
+                lastMonthNumber > 0 && strcmp (lastTicker, currentTicker))
+            alldata{col_capexq}(i) = alldata{col_capexq}(lastAccountingRow);
+            alldata{col_cashflowq}(i) = alldata{col_cashflowq}(lastAccountingRow);
+            alldata{col_cashflowshareq}(i) = alldata{col_cashflowshareq}(lastAccountingRow);
+            alldata{col_dividendq}(i) = alldata{col_dividendq}(lastAccountingRow);
+            alldata{col_epsq}(i) = alldata{col_epsq}(lastAccountingRow);
+            alldata{col_netincomeq}(i) = alldata{col_netincomeq}(lastAccountingRow);
+            alldata{col_rndq}(i) = alldata{col_rndq}(lastAccountingRow);
+            alldata{col_sharesq}(i) = alldata{col_sharesq}(lastAccountingRow);
+            alldata{col_totalassetsq}(i) = alldata{col_totalassetsq}(lastAccountingRow);
+            alldata{col_totalliabilitiesq}(i) = alldata{col_totalliabilitiesq}(lastAccountingRow);
+        end
+    end
+end
+
+capExpRatio = alldata{col_capexq} ./ alldata{col_totalassetsq};
+capExpRatio = lag(capExpRatio, minAccountingLag);
+
+debtRatio = alldata{col_totalliabilitiesq} ./ alldata{col_totalassetsq};
+debtRatio = lag(debtRatio, minAccountingLag);
+
+rndRatio = alldata{col_rndq} ./ alldata{col_totalassetsq};
+rndRatio = lag (rndRatio, minAccountingLag);
+
+roe = alldata{col_netincomeq} ./ alldata{col_totalassetsq};
+roe = lag (roe, minAccountingLag);
+
+laggedTotalAsset = lag (alldata{col_totalassetsq}, minAccountingLag);
+laggedCashFlow = lag (alldata{col_cashflowq}, minAccountingLag);
+laggedDividend = lag (alldata{col_dividendq}, minAccountingLag);
+laggedEps = lag (alldata{col_epsq}, minAccountingLag);
+
+price2book = price ./ laggedTotalAsset;
+price2cashFlow = price ./ laggedCashFlow;
+price2dividend = price ./ laggedDividend;
+price2earnings = price ./ laggedEps;
+
+%Trim all arrays, first minAccountingLag rows are useless
+priceReturn = priceReturn (minAccountingLag+1:NumRow);
+capExpRatio = capExpRatio (minAccountingLag+1:NumRow);
+debtRatio = debtRatio (minAccountingLag+1:NumRow);
+rndRatio = rndRatio (minAccountingLag+1:NumRow);
+roe = roe (minAccountingLag+1:NumRow);
+price2book = price2book (minAccountingLag+1:NumRow);
+price2cashFlow = price2cashFlow (minAccountingLag+1:NumRow);
+price2dividend = price2dividend (minAccountingLag+1:NumRow);
+price2earnings = price2earnings (minAccountingLag+1:NumRow);
+
+
+%%%%%%%%%%%% Regression test on factors
+[y,x] = keepValidPair(priceReturn, capExpRatio);
+result=ols(y,[x ones(size(x))]);
+disp('Capital Expenditure / Total Asset Ratio');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, debtRatio);
+result=ols(y,[x ones(size(x))]);
+disp('Debt / Total Asset Ratio');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, rndRatio);
+result=ols(y,[x ones(size(x))]);
+disp('RND / Total Asset Ratio');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, roe);
+result=ols(y,[x ones(size(x))]);
+disp('Return on Equity');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, price2book);
+result=ols(y,[x ones(size(x))]);
+disp('Price to Book Ratio');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, price2cashFlow);
+result=ols(y,[x ones(size(x))]);
+disp('Price to Cash Flow');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, price2dividend);
+result=ols(y,[x ones(size(x))]);
+disp('Price to Dividend');
+result.beta
+
+[y,x] = keepValidPair(priceReturn, price2earnings);
+result=ols(y,[x ones(size(x))]);
+disp('Price to Earnings Ratio');
+result.beta
